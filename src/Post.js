49d63e84561b9d8ca066b88cf3414578e6f4f71f
Андрей Федорчук <streed1rt@yandex.ru>
2020-01-18 18:36:51 +0300

    second commit

---
+++ src/Post.js
@@ -0,0 +1,13 @@
+export default class Post {
+  constructor(title) {
+    this.title = title;
+    this.data = new Date();
+  }
+
+  toString() {
+    return JSON.stringify({
+      title: this.title,
+      date: this.data.toJSON()
+    })
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Post from './Post'
+const post = new Post('Webpack Post Title')
+console.log(post.toString());
+console.log(post.toString());
